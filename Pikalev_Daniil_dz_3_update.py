d89b70979eeaeb8ea23735da6ca931d51744f615
Daniel <pikalevdo@yandex.ru>
2021-03-14 18:08:25 +0300

    Complete the third task in a different way

---
+++ Pikalev_Daniil_dz_3_update.py
@@ -0,0 +1,7 @@
+for number in range(1, 21):
+    if number == 1:
+        print(str(number) + ' процент')
+    elif number > 1 and number < 5:
+        print(str(number) + ' процента')
+    elif number >=5:
+        print(str(number) + ' процентов')
